e98d75e1ed269c2bb9cfc21ebb769a7b44b708e9
AgustinDiLeone <agustindileone@gmail.com>
2023-06-13 22:08:15 -0300

    listo

--- main.py
+++ main.py
@@ -1,39 +1,58 @@
 import pygame, random, sys
 from modo import *
 from personaje import personaje
+from config import *
+from plataformas import plataforma
 
-
-W,H = 800,600
-screen_size = (W,H)
-FPS = 20
 
 pygame.init()
-RELOJ = pygame.time.Clock()
-PANTALLA = pygame.display.set_mode(screen_size)
+
+PANTALLA = pygame.display.set_mode(SIZE_SCREEN)
 pygame.display.set_caption("BEN 10")
+RELOJ = pygame.time.Clock()
 
 #FONDO ############################################
 
 fondo = pygame.image.load(r"RECURSOS\fondo.png")
-fondo = pygame.transform.scale(fondo, screen_size)
+fondo = pygame.transform.scale(fondo, SIZE_SCREEN)
 PANTALLA.blit(fondo, (0,0))
 
-#ICONO 
+#ICONO  ###############################################################3
 icono = pygame.image.load("RECURSOS\ico.png")
 pygame.display.set_icon(icono)
 
-#MUSICA
+#MUSICA ############################################################3
 pygame.mixer.music.load("RECURSOS\musica.mp3")
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.1)
 
-#fuente
+#fuente ##############################################################
 fuente = pygame.font.SysFont("Arial",30)
 texto = fuente.render("mi primer juego", False, "black")
 
-#homero
-ben = personaje("RECURSOS\derecha.png", (100,150), 200,300)
+#ben #################################################################
 
+ben = personaje("RECURSOS\derecha.png", (100,150), 200, 300, 10, 15, -21)
+
+
+
+correr = [
+    pygame.image.load("RECURSOS/derecha.png"),
+    pygame.image.load("RECURSOS/izquierda.png")
+]
+quieto = [pygame.image.load("RECURSOS/derecha.png")]
+
+#PLATAFORMA ########################################################3
+piso = plataforma("RECURSOS\images.jpg", (WIDTH,200),0,HEIGHT-200)
+
+plataformas = plataforma("RECURSOS\images.jpg", (400,100),400,400)
+
+pygame.draw.rect(PANTALLA, "brown",plataformas.rect ,2)
+pygame.draw.rect(PANTALLA, "grey",piso.rect ,2)
+
+
+
+#####################################################33
 while True:
     RELOJ.tick(FPS)
     lista_eventos = pygame.event.get()
@@ -48,20 +67,25 @@ while True:
     if keys[pygame.K_ESCAPE]:
         pygame.quit()
         sys.exit(0)
-    elif keys[pygame.K_RIGHT]:
-        que_hace = "derecha"
-    elif keys[pygame.K_LEFT]:
-        que_hace = "izquierda"
-    elif keys[pygame.K_UP]:
-        que_hace = "saltar"
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        ben.mover_derecha()
+    elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        ben.mover_izquierda()
+    elif keys[pygame.K_UP] or keys[pygame.K_w]  or keys[pygame.K_SPACE]:
+        ben.saltar()
     else:
-        que_hace = "quieto"
+        #ben.quieto()
+        pass
 
+    PANTALLA.blit(fondo, (0,0))
+    PANTALLA.blit(ben.imagen, ben.rect)
+    PANTALLA.blit(plataformas.imagen, plataformas.rect)
+    
     if get_mode() == True:
         pygame.draw.rect(PANTALLA, "blue",ben.rect ,2)
         pygame.draw.rect(PANTALLA, "red",ben.rect_botton,2)
         pygame.draw.rect(PANTALLA, "black",ben.rect_left,2)
         pygame.draw.rect(PANTALLA, "green",ben.rect_right,2)
         pygame.draw.rect(PANTALLA, "purple",ben.rect_top,2)
-
+    
     pygame.display.update()
--- personaje.py
+++ personaje.py
@@ -1,9 +1,10 @@
 import pygame
+from  config import *
 
 class personaje():
-    def __init__(self, imagen, size, x, y) -> None:
+    def __init__(self, imagen, size:tuple, x, y, velocidad,potencia_salto, gravedad) -> None:
         self.imagen = pygame.image.load(imagen)
-        self.imagen = pygame.transform.scale(self.imagen,(size))
+        self.imagen = pygame.transform.scale(self.imagen,size)
         self.imagen_invertida = pygame.transform.flip(self.imagen, True, False)
         self.rect = self.imagen.get_rect()
         self.rect.x = x
@@ -12,3 +13,45 @@ class personaje():
         self.rect_left = pygame.Rect(self.rect.left, self.rect.top,6, self.rect.height)
         self.rect_top = pygame.Rect(self.rect.left, self.rect.top, self.rect.width, 6)
         self.rect_botton = pygame.Rect(self.rect.left, self.rect.bottom -6, self.rect.width, 6)
+        self.lado_personaje = [self.rect, self.rect_botton, self.rect_left, self.rect_right, self.rect_top]
+        self.velocidad = velocidad
+        self.potencia_salto = potencia_salto
+        self.gravedad = gravedad
+
+    def mover_personaje(self, velocidad):
+        for lado in range(len(self.lado_personaje)):
+            self.lado_personaje[lado].x += velocidad
+
+    def animar_personaje(self, pantalla, rectangulo):
+        largo = len(acciones)
+        if contador_pasos >= largo:
+            contador_pasos = 0
+
+        pantalla.blit(acciones[contador_pasos],rectangulo)
+        contador_pasos += 1
+
+    def aplicar_gravedad(self):
+        self.gravedad += 1
+        self.rect.y += self.gravedad
+        if self.rect.botton >= 300:
+            self.rect.bottom = 300
+
+        
+
+    def mover_derecha(self):
+        if self.rect.x < WIDTH-self.rect.width:
+            self.mover_personaje(self.velocidad)
+        #self.animar_personaje()
+
+    def quieto(self):
+        self.animar_personaje()
+
+    def mover_izquierda(self):
+        if self.rect.x > 0:
+            self.mover_personaje(self.velocidad*-1)
+        #self.animar_personaje()
+
+    def saltar(self):
+        if self.rect.y > 0:
+            for lado in range(len(self.lado_personaje)):
+                self.lado_personaje[lado].y -= self.potencia_salto
